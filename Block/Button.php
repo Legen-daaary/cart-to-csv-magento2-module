44498ebe8373ff0a882eb4db597d99b033ff425c
Legen-daaary <fut.tibor@gmail.com>
2023-05-20 14:27:32 +0200

    Add getButtonCaption method to the Button class

--- Block/Button.php
+++ Block/Button.php
@@ -6,5 +6,13 @@ use Magento\Framework\View\Element\Template;
 
 class Button extends Template
 {
+    private string $buttonCaption = "Download Cart";
 
+    /**
+     * @return string
+     */
+    public function getButtonCaption(): string
+    {
+        return $this->buttonCaption;
+    }
 }
