99ae74ce9318be110ba68f9bc32abc7e9964c10c
Legen-daaary <fut.tibor@gmail.com>
2023-05-21 12:30:14 +0200

    Implement BL in CartDownloadSupervisorPlugin.php

--- Plugin/CartDownloadSupervisorPlugin.php
+++ Plugin/CartDownloadSupervisorPlugin.php
@@ -2,7 +2,32 @@
 
 namespace Legendaaary\CartToCSV\Plugin;
 
+use Closure;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Store\Model\ScopeInterface;
+
 class CartDownloadSupervisorPlugin
 {
+    protected ScopeConfigInterface $scopeConfig;
 
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+
+    public function aroundCreate($subject, Closure $proceed, $type)
+    {
+        if ($type === 'Legendaaary\CartToCSV\Controller\Download\Index') {
+            $isModuleEnabled = $this->scopeConfig->isSetFlag('legendaaary_cart_to_csv/general/enable',
+                ScopeInterface::SCOPE_STORE);
+
+            if (!$isModuleEnabled) {
+                throw new NotFoundException(__('Page not found.'));
+            }
+        }
+        return $proceed($type);
+    }
 }
